06c480d347e2f0c2a302fb247e67a7433da1785d
Olaf Mersmann <olafm@p-value.net>
2011-01-06 22:32:13 +0000

    Add add_normal_noise() function.
    This is used to create a noisy test problem from a regular test
    problem.

---
+++ examples/noise.R
@@ -0,0 +1,5 @@
+require("soobench")
+
+## Create noisy sphere function:
+f <- add_normal_noise(f_sphere, 1)
+
---
+++ skel/R/noise.R
@@ -0,0 +1,32 @@
+##' Add normal noise to a test function.
+##'
+##' @param f Test function.
+##' @param sd Standard deviation of the noise.
+##' @return A noisy version of \code{f}.
+##' @export
+add_normal_noise <- function(f, sd=1) {
+  stopifnot(is.function(f))
+  force(sd)
+  .tmp <- function(x, ...)
+    f(x, ...) + rnorm(1, mean=0, sd=sd)
+  class(.tmp) <- "normal_noise_function"
+  .tmp
+}
+
+##' @S3method lower_bounds normal_noise_function
+##' @method lower_bounds normal_noise_function
+##' @rdname bounds.Rd
+lower_bounds.normal_noise_function <- function(x, dim)
+  lower_bounds(environment(x)$f, dim)
+
+##' @S3method upper_bounds normal_noise_function
+##' @method upper_bounds normal_noise_function
+##' @rdname bounds.Rd
+upper_bounds.normal_noise_function <- function(x, dim)
+  upper_bounds(environment(x)$f, dim)
+
+##' @S3method global_minimum normal_noise_function
+##' @method global_minimum normal_noise_function
+##' @rdname global_minimum.Rd
+global_minimum.normal_noise_function <- function(x, dim, ...)
+  global_minimum(environment(x)$f, dim, ...)
