0a61112d7922e4171afe0130f228b021540dc1a0
Olaf Mersmann <olafm@p-value.net>
2011-01-12 12:19:00 +0000

    Add a helper plot function.
    plot_2dfunction() is a helper function to quickly visualize a test
    function in 2D.

---
+++ skel/R/plot_2dfunction.R
@@ -0,0 +1,17 @@
+##' Plot a test function in 2D.
+##'
+##' @param f Function to plot.
+##' @param xlim Limit of x1.
+##' @param ylim Limit of x2.
+##' @param n Number of places to sample the function.
+##' @export
+plot_2dfunction <- function(f, xlim, ylim, n=1024, ...) {
+  k <- floor(sqrt(n))
+  x1 <- seq(xlim[1], xlim[2], length.out=k)
+  x2 <- seq(ylim[1], ylim[2], length.out=k)
+  X <- expand.grid(x1, x2)
+  y <- apply(X, 1, f)
+  dim(y) <- c(k, k)
+  image(x1, x2, y, col=terrain.colors(200))
+  contour(x1, x2, y, add=TRUE, ...)
+}
