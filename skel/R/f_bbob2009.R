3941d3268218b0f5c6c231efc40dafe6fda2ae2c
Olaf Mersmann <olafm@p-value.net>
2011-07-27 01:11:06 +0000

    Refactor BBOB 2009 test function generator.

--- skel/R/f_bbob2009.R
+++ skel/R/f_bbob2009.R
@@ -13,26 +13,13 @@ bbob2009_function <- function(dimensions, fid, iid) {
   f <- function(x) {}
   body(f) <- substitute(.Call(do_bbob_eval, fid, iid, x),
                         list(fid=fid, iid=iid))
-
-  fn <- soo_function(name=sprintf("BBOB 2009 function %i (instance %i)", fid, iid),
-                     id=sprintf("bbob2009-%02i-%02i-%id", fid, iid, dimensions),
-                     fun=f,
-                     dimensions=dimensions,
-                     lower_bounds=rep(-5, dimensions),
-                     upper_bounds=rep(5, dimensions),
-                     best_par=opt[[1]],
-                     best_value=opt[[2]])
-  attr(fn, "fid", fid)
-  attr(fn, "iid", iid)
-  class(fn) <- c("bbob2009_function", class(fn))
-  fn
-}
-
-##' @S3method function_name bbob2009_function
-##' @method function_name bbob2009_function
-##' @rdname function_name.Rd
-function_name.bbob2009_function <- function(fn) {
-  fid <- attr(fn, "fid")
-  iid <- attr(fn, "iid")
-  sprintf("BBOB 2009 test function Nr. %i (instance %i)", fid, iid)
+  
+  soo_function(name=sprintf("BBOB 2009 problem %i (instance %i)", fid, iid),
+               id=sprintf("bbob2009-%02i-%02i-%id", fid, iid, dimensions),
+               fun=f,
+               dimensions=dimensions,
+               lower_bounds=rep(-5, dimensions),
+               upper_bounds=rep(5, dimensions),
+               best_par=opt[[1]],
+               best_value=opt[[2]])
 }
