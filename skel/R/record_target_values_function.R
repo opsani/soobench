2fb9714a946a1b0e7463f915554fee43da82853b
Bernd Bischl <bernd_bischl@gmx.de>
2012-04-06 02:13:20 +0000

    included record_target_values_function and first_hitting_times. tests and checks all passed, but currently not exported.

---
+++ skel/R/record_target_values_function.R
@@ -0,0 +1,76 @@
+##' Return a new function which is identical to the \code{soofunction}
+##' passed in except that all visited target vales are recorded.
+##' 
+##' @param fun A test function (class \code{soo_function}).
+##'
+##' @examples
+##' f <- soobench:::record_target_values_function(sphere_function(1))
+##' soobench:::target_values(f)
+##' sapply(c(2,3,1), f)
+##' y <- soobench:::target_values(f)
+##' soobench:::first_hitting_times(y, c(1, 7), minimize=FALSE) 
+##' soobench:::reset_target_values(f)
+##' soobench:::target_values(f)
+##'
+##' @seealso \code{\link{target_values}},
+##'          \code{\link{reset_target_values}},
+##'          \code{\link{first_hitting_times}}
+# FIXME: remove ::: when exported in example
+record_target_values_function <- function(fun) {            
+  force(fun)
+  stopifnot("soo_function" %in% class(fun))
+  if ("record_target_values_function" %in% class(fun))
+    stop("Function already is of type 'record_target_values_function'.")
+    
+  target_values <- numeric(0L)
+  
+  cfun <- function(x, ...) {
+    y <- fun(x, ...)
+    target_values <<- c(target_values, y)
+    y
+  }
+  attributes(cfun) <- attributes(fun)
+  class(cfun) <- c("record_target_values_function", class(fun))
+  cfun
+}
+
+##' Return the internally recorded target values for a test function.
+##'
+##' @param fun A \code{record_target_values_function} as returned by
+##'   \code{\link{record_target_values_function}}.
+##' 
+##' @return The currently recorded target values.
+target_values <- function(fun) {
+  stopifnot("record_target_values_function" %in% class(fun))
+  environment(fun)$target_values
+}
+
+##' Reset the internal target values of a test function.
+##' 
+##' @param fun A \code{record_target_values_function} as returned by
+##'   \code{\link{record_target_values_function}}.
+##'
+##' @return Recorded target values before reset.
+reset_target_values <- function(fun) {
+  stopifnot("record_target_values_function" %in% class(fun))
+  ee <- environment(fun)
+  last <- ee$target_values
+  ee$target_values <- numeric(0L)
+  last
+}
+
+##' Return numerical vector of first hitting times, counted in 
+##' function evaluations.
+##' 
+##' @param target_values Numerical vector of recorded target values.
+##' @param target_levels Numerical vector of target levels which should
+##'   ideally be reached.
+##' @param minimize Is the function to be minimized that produced the target values?
+first_hitting_times <- function(target_values, target_levels, minimize=TRUE) {
+  if (!minimize) {
+    target_values <- -1*target_values 
+    target_levels <- -1*target_levels 
+  }
+  sapply(target_levels, function(lev) which(target_values <= lev)[1L])
+}
+
