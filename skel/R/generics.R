d8d3d23b9da5baade84490bd22e190a0fd2d0b33
Olaf Mersmann <olafm@p-value.net>
2013-03-07 14:54:58 +0000

    Add a random_parameter function.
    RATIOANL: Often one wants just a single parameter setting so instead
    of calling
    
      random_parameters(1, fn)[,1]
    
    add a convenience function that does exactly that.

--- skel/R/generics.R
+++ skel/R/generics.R
@@ -60,23 +60,23 @@ soo_function <- function(name, id, fun, dimensions,
 ##' @param fn Function to query.
 ##' @return Vector of lower or upper bounds of test function.
 ##' @export
-##' @rdname bounds.Rd
+##' @rdname bounds
 lower_bounds <- function(fn)
   UseMethod("lower_bounds")
 
 ##' @export
-##' @rdname bounds.Rd
+##' @rdname bounds
 upper_bounds <- function(fn)
   UseMethod("upper_bounds")
 
 ##' @S3method lower_bounds soo_function
 ##' @method  lower_bounds soo_function
-##' @rdname bounds.Rd
+##' @rdname bounds
 lower_bounds.soo_function <- function(fn) attr(fn, "lower_bounds")
 
 ##' @S3method upper_bounds soo_function
 ##' @method  upper_bounds soo_function
-##' @rdname bounds.Rd
+##' @rdname bounds
 upper_bounds.soo_function <- function(fn) attr(fn, "upper_bounds")
 
 ##' Retrieve the global minimum of a function.
@@ -89,13 +89,13 @@ upper_bounds.soo_function <- function(fn) attr(fn, "upper_bounds")
 ##' value of the global minimum.
 ##' 
 ##' @export
-##' @rdname global_minimum.Rd
+##' @rdname global_minimum
 global_minimum <- function(fn)
   UseMethod("global_minimum")
 
 ##' @S3method global_minimum soo_function
 ##' @method  global_minimum soo_function
-##' @rdname global_minimum.Rd
+##' @rdname global_minimum
 global_minimum.soo_function <- function(fn)
   list(par=attr(fn, "best_par"), value=attr(fn, "best_value"))
 
@@ -104,13 +104,13 @@ global_minimum.soo_function <- function(fn)
 ##' @param fn Function to name.
 ##' @return Name of function.
 ##' @export
-##' @rdname function_name.Rd
+##' @rdname function_name
 function_name <- function(fn)
   UseMethod("function_name")    
 
 ##' @S3method function_name soo_function
 ##' @method function_name soo_function
-##' @rdname function_name.Rd
+##' @rdname function_name
 function_name.soo_function <- function(fn) {
   basename <- attr(fn, "name")
   dim <- attr(fn, "dimensions")
@@ -119,7 +119,7 @@ function_name.soo_function <- function(fn) {
 
 ##' @S3method function_name soo_function_generator
 ##' @method function_name soo_function_generator
-##' @rdname function_name.Rd
+##' @rdname function_name
 function_name.soo_function_generator <- function(fn)
   attr(fn, "name")
 
@@ -130,19 +130,19 @@ function_name.soo_function_generator <- function(fn)
 ##' @return ID of function. Guaranteed to be unique among all test
 ##' functions.
 ##' @export
-##' @rdname function_id.Rd
+##' @rdname function_id
 function_id <- function(fn)
   UseMethod("function_id")    
 
 ##' @S3method function_id soo_function
 ##' @method function_id soo_function
-##' @rdname function_id.Rd
+##' @rdname function_id
 function_id.soo_function <- function(fn)
   attr(fn, "id")
 
 ##' @S3method function_id soo_function_generator
 ##' @method function_id soo_function_generator
-##' @rdname function_id.Rd
+##' @rdname function_id
 function_id.soo_function_generator <- function(fn) {
   attr(fn, "id")
 }
@@ -164,17 +164,17 @@ is_function_generator <- function(fn)
 ##' @return Expected length of first argument. I.e. the size of the
 ##' parameter space of the function \code{fn}.
 ##' @export
-##' @rdname number_of_parameters.Rd
+##' @rdname number_of_parameters
 number_of_parameters <- function(fn)
   UseMethod("number_of_parameters")
 
 ##' @S3method number_of_parameters soo_function
 ##' @method number_of_parameters soo_function
-##' @rdname number_of_parameters.Rd
+##' @rdname number_of_parameters
 number_of_parameters.soo_function <- function(fn)
   attr(fn, "dimensions")  
 
-##' @title Generate random parameters for a given function.
+##' @title Generate random parameter(s) for a given function.
 ##'
 ##' Given a test function \code{fn}, generate \code{n} random
 ##' parameter settings for that function.
@@ -182,8 +182,10 @@ number_of_parameters.soo_function <- function(fn)
 ##' @param n Number of parameters to generate.
 ##' @param fn Test function.
 ##'
-##' @return A matrix containing the parameter settings in the
-##' \emph{columns} of the matrix.
+##' @return For \code{random_parameters}, a matrix containing the
+##' parameter settings in the \emph{columns} of the
+##' matrix. \code{random_parameter} returns a numeric vector with a
+##' single parameter setting for the given function.
 ##'
 ##' @examples
 ##' fn <- generate_ackley_function(10)
@@ -192,8 +194,14 @@ number_of_parameters.soo_function <- function(fn)
 ##' y <- fn(X)
 ##' 
 ##' @export
+##' @rdname random_parameters
 random_parameters <- function(n, fn)
   UseMethod("random_parameters", fn)
+
+##' @export
+##' @rdname random_parameters
+random_parameter <- function(fn) 
+  random_parameters(1, fn)[,1]
 
 ##' @S3method random_parameters soo_function
 ##' @method random_parameters soo_function
