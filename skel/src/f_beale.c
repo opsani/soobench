d4a1ea2bf17693f4a1467375fd2e35950900d25c
Olaf Mersmann <olafm@p-value.net>
2013-03-22 14:27:03 +0000

    Fix bug in Beale implementation.

--- skel/src/f_beale.c
+++ skel/src/f_beale.c
@@ -4,9 +4,9 @@ const double f_beale(const double *x, const size_t n) {
     const double x1 = x[0];
     const double x2 = x[1];
 
-    const double c1 = 1 - x1;
-    const double c2 = 1 - x1 * x1;
-    const double c3 = 1 - x1 * x1 * x1;
+    const double c1 = 1 - x2;
+    const double c2 = 1 - x2 * x2;
+    const double c3 = 1 - x2 * x2 * x2;
 
     const double c4 = 1.5 - x1 * c1;
     const double c5 = 2.25 - x1 * c2;
