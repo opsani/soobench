e174a7eb6b075e6c63ed4f1db04bce0b91fbe376
Olaf Mersmann <olafm@p-value.net>
2013-09-04 09:48:48 +0000

    Add C version of discus function.

--- skel/R/f_discus.R
+++ skel/R/f_discus.R
@@ -1,23 +1,26 @@
 ##' Discus test function generator.
 ##'
-##' The discus test function is similar to a high condition ellipsoid. It is defined as
+##' The discus test function is similar to a high condition
+##' ellipsoid. It is defined as
 ##'
 ##'   \deqn{f(x) = 10^6 x_1^2 + \sum_{i=2}^n x_i^2}
 ##' 
 ##' @param dimensions [\code{integer(1)}] Size of parameter space.
 ##' @return A \code{soo_function}.
 ##' @export
-generate_discus_function <- function(dimensions)
+##' @useDynLib soobench do_f_discus
+generate_discus_function <- function(dimensions) {
+  stopifnot(dimensions >= 2)
   soo_function(name="Discus",
                id=sprintf("discus-%id", dimensions),
-               fun=function(x, ...) 1e6 * x[1]^2 + sum(x[-1]*x[-1]),
+               fun=function(x, ...) .Call(do_f_discus, x),
                dimensions=dimensions,
                lower_bounds=rep(-32.768, dimensions),
                upper_bounds=rep(32.768, dimensions),
                best_par=rep(0, dimensions),
                best_value=0)
+}
 
-               
 class(generate_discus_function) <- "soo_function_generator"
 attr(generate_discus_function, "id") <- "discus"
 attr(generate_discus_function, "name") <- "Discus test function"
