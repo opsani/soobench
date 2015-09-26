a8145b9351d359c12fc4092580ca8903f022c84e
Olaf Mersmann <olafm@p-value.net>
2015-09-26 11:52:58 +0200

    Improve numerical accuracy.

--- src/f_number_of_active_faces.c
+++ src/f_number_of_active_faces.c
@@ -7,12 +7,12 @@ const double f_number_of_active_faces(const double *x, const size_t n) {
   size_t i;
   /* Calculate both sum as well as maximum in one pass: */
   for (i = 0; i < n; ++i) {
-    const double g = log(fabs(x[i]) + 1.0);
+    const double g = log1p(fabs(x[i]));
     if (g > max)
       max = g;
 
     sum += x[i];
   }
-  const double gsum = log(fabs(sum) + 1.0);
+  const double gsum = log1p(fabs(sum));
   return gsum > max ? gsum : max;
 }
